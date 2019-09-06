e9d8572741b5a0b46dd09f3de9ebd3eccf3f0713
Angela <8798027+angelabauer@users.noreply.github.com>
2019-09-06 12:48:42 +0100

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Angela Yu on 06/09/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
