3eaea3c76fde95926124807831d71c189e49383f
Angela <8798027+angelabauer@users.noreply.github.com>
2019-09-17 15:16:17 +0100

    Update code

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -10,7 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
     }
 }
 
